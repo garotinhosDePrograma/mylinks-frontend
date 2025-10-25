dabb646e975f89b98e8e811b28978404d4873f5d
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-25 18:35:47 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nome do cache (você pode mudar a versão quando atualizar o site)
-const CACHE_NAME = "mylinks-cache-v1.39";
+const CACHE_NAME = "mylinks-cache-v1.40";
 
 // Lista de arquivos que serão armazenados no cache
 const urlsToCache = [
