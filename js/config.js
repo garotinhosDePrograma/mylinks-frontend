3cdc50eb3c63898719858093da74bbdf89f13872
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-25 17:03:19 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    API_URL: "https://pygre.onrender.com",
+    DEFAULT_AVATAR: "assets/default-avatar.png",
+    TOKEN_EXPIRATION_TIME: 60 * 60 * 1000, // 1 hora em ms
+};
+
+window.CONFIG = CONFIG;
