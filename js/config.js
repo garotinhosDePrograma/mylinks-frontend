8e51623f21e2a1dbce20271363c88619b4706e64
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-29 19:16:07 -0300

    a

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_URL: "https://mylinks-em4k1t2cx-mylinks-tree.vercel.app/",
+    API_URL: "https://mylinks-api.vercel.app",
     DEFAULT_AVATAR: "assets/default-avatar.png",
     TOKEN_EXPIRATION_TIME: 60 * 60 * 1000,
 };
