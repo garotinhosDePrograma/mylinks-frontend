26aced9fbeac2f705c2c666d8fb5c7420f6d38e5
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-12-17 16:23:43 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -309,6 +309,7 @@ function toggleLoading(formType, mostrar) {
 }
 
 function mostrarMensagem(texto, tipo = 'info', duracao = 5000) {
+    const mensagem = document.getElementById("mensagem");
     mensagem.textContent = texto;
     mensagem.className = '';
     mensagem.setAttribute('role', 'alert');
